5ed76034d4ad1444dd594bb24fc24d135bd7a70f
Kalab Smith <kalabsb@me.com>
2025-02-11 22:48:18 -0600

    update

--- client/src/interfaces/EventData.tsx
+++ client/src/interfaces/EventData.tsx
@@ -2,7 +2,7 @@ export interface EventData {
   id?: number;
   location: string;
   date: string;
-  moonPhase: string;
+  moon_phase: string;
   weather?: {
     weather: { main: string; icon: string }[];
     main: { temp: number; humidity: number };
--- client/src/interfaces/SearchResult.tsx
+++ client/src/interfaces/SearchResult.tsx
@@ -1,7 +1,7 @@
 export interface SearchResult {
   location: string;
   date: string;
-  moonPhase: string;
+  moon_phase: string;
   weather?: {
     weather: { main: string; icon: string }[];
     main: { temp: number; humidity: number };
